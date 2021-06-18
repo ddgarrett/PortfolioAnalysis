66209a576392432853de65be9f7d98b2d99a5f7e
Doug <doug@anetsc.com>
2021-06-17 17:02:38 -0700

    Extend Example

--- stockscenario_test.go
+++ stockscenario_test.go
@@ -97,16 +97,43 @@ func TestRun_Part1(t *testing.T) {
 func ExampleCalcResults() {
 	fxaix, _ := NewStock("FXAIX")
 
-	sc := NewStockScenario("2020-01-01", "2020-12-31")
+	years := []string{"2016", "2017", "2018", "2019", "2020"}
+
+	for _, year := range years {
+		sc := NewStockScenario(year+"-01-01", year+"-12-31")
+		sc.AddStock(fxaix, 1)
+		sc.CalcResults(10000)
+		printResults(sc)
+	}
+
+	sc := NewStockScenario("2011-05-31", "2021-05-31")
 	sc.AddStock(fxaix, 1)
 	sc.CalcResults(10000)
+	printResults(sc)
 
+	// output: Stock FXAIX, StartDate: 2016-01-01, EndDate: 2016-12-31
+	// 	StartAmt: 10,000.00 EndAmt: 11,140.89 PctChange: 11.4089%
+	//
+	// Stock FXAIX, StartDate: 2017-01-01, EndDate: 2017-12-31
+	// 	StartAmt: 10,000.00 EndAmt: 12,159.01 PctChange: 21.5901%
+	//
+	// Stock FXAIX, StartDate: 2018-01-01, EndDate: 2018-12-31
+	// 	StartAmt: 10,000.00 EndAmt: 9,500.10 PctChange: -4.9990%
+	//
+	// Stock FXAIX, StartDate: 2019-01-01, EndDate: 2019-12-31
+	// 	StartAmt: 10,000.00 EndAmt: 13,131.70 PctChange: 31.3170%
+	//
+	// Stock FXAIX, StartDate: 2020-01-01, EndDate: 2020-12-31
+	// 	StartAmt: 10,000.00 EndAmt: 11,839.52 PctChange: 18.3952%
+	//
+	// Stock FXAIX, StartDate: 2011-05-31, EndDate: 2021-05-31
+	// 	StartAmt: 10,000.00 EndAmt: 37,253.38 PctChange: 272.5338%
+}
+
+func printResults(sc *StockScenario) {
 	p := message.NewPrinter(language.English)
 	p.Printf("Stock %s, StartDate: %s, EndDate: %s\n", "FXAIX", sc.StartDate, sc.EndDate)
 
-	p.Printf("\tStartAmt: %.2f EndAmt: %.2f PctChange: %.4f%%\n",
+	p.Printf("\tStartAmt: %.2f EndAmt: %.2f PctChange: %.4f%%\n\n",
 		sc.StartAmt, sc.EndAmt, sc.PctChange*100)
-
-	// output: Stock FXAIX, StartDate: 2020-01-01, EndDate: 2020-12-31
-	//	StartAmt: 10,000.00 EndAmt: 11,839.52 PctChange: 18.3952%
 }
